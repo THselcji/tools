091804b011bc2f7a1de68d3de64bec306909a489
THselcji <selcji16@gmail.com>
2025-12-14 19:00:20 +0800

    Update Stealtools.lua

--- Stealtools.lua
+++ Stealtools.lua
@@ -1,4 +1,4 @@
 -- [STEAL TOOLS v.1.00]
--- Script just copy below:
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.lol/refs/heads/main/loader.lua", true))()
+-- 32&64bit supported, lightweight loader for low end devices. This script has dsync, tp base and steal floor etc.
+-- script, just copy beliow:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.lol/refs/heads/main/loader.lua",true))()
