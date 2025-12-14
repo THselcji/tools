31b44b97530a6935b52869143d9aec43594d4f7a
THselcji <selcji16@gmail.com>
2025-12-14 18:54:01 +0800

    Create Stealtools.lua

---
+++ Stealtools.lua
@@ -0,0 +1,4 @@
+-- [STEAL TOOLS v.1.00]
+-- Script just copy below:
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.lol/refs/heads/main/loader.lua", true))()
