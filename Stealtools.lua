696fbbd6da0dc00c513c3c6b8482a8097fa410fc
THselcji <selcji16@gmail.com>
2025-12-14 19:15:59 +0800

    Update Stealtools.lua

--- Stealtools.lua
+++ Stealtools.lua
@@ -1,6 +1,9 @@
--- [STEAL TOOLS v.1.00]
+-- [STEAL TOOLS v.1.00]  
+-- discord.gg/stoolshuh
+
+--[NEW UPD!]
 -- 32&64bit supported and has safeCombi, lightweight loader for low end devices.
 -- steal a brainrots Perm Dysnc, tp to base and steal floor, Quantum Dysnc, light dysnc, invisible, ESP highest M/s and etc.
 
--- script here:
+-- script here, just copy all:
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.lol/refs/heads/main/loader.lua",true))()
